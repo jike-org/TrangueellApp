ec716e9cf7e435e015c45e36dad3ed2736b9980a
alesdematteo01 <62564507+alesdematteo01@users.noreply.github.com>
2022-05-10 14:30:39 +0200

    Initial Commit

---
+++ TrangueellApp/TrangueellAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  TrangueellAppApp.swift
+//  TrangueellApp
+//
+//  Created by Alessandra De Matteo on 10/05/22.
+//
+
+import SwiftUI
+
+@main
+struct TrangueellAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
