2cd49c29b639029483cebd8b076c6509fac26a45
alesdematteo01 <62564507+alesdematteo01@users.noreply.github.com>
2022-05-11 17:50:30 +0200

    Update changes

--- TrangueellApp/DreamView.swift
+++ TrangueellApp/DreamView.swift
@@ -9,12 +9,32 @@ import SwiftUI
 import CoreData
 
 struct DreamView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+
     var dream: Dream
+    @State var dreamText: String
     
     var body: some View {
         Text("Dream at \(dream.dreamTitle!)")
-        Text("\(dream.dreamDescription!)")
+        TextField("", text: $dreamText)
+            .onChange(of: dreamText) { newValue in
+                updateItem()
+            }
         Text("\(dream.dreamDate!.formatted())")
+    }
+    
+    private func updateItem(){
+        withAnimation {
+            dream.dreamDescription = dreamText
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
     }
 }
 
